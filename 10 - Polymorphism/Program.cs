76f95a9389f8b1b68c1f76c868217213f915303e
Aman Ahmed <aman16.aa17@gmail.com>
2022-11-11 14:31:08 +0100

    Turned off: Interfaces vs Abstract Classes

--- 10 - Polymorphism/Program.cs
+++ 10 - Polymorphism/Program.cs
@@ -50,30 +50,30 @@ namespace _10___Polymorphism
             // M3 myM3 = new M3(260, "Red", "M3 Super Turbo");
             // myM3.Repair();
 
-            Shape[] shapes = { new Sphere(4), new Cube(3) };
+            // Shape[] shapes = { new Sphere(4), new Cube(3) };
 
-            foreach (Shape shape in shapes)
-            {
-                shape.GetInfo();
-                Console.WriteLine("{0} has a Volume of {1}", shape.Name, shape.Volume());
+            // foreach (Shape shape in shapes)
+            // {
+            //     shape.GetInfo();
+            //     Console.WriteLine("{0} has a Volume of {1}", shape.Name, shape.Volume());
 
-                Cube iceCube = shape as Cube;
+            //     Cube iceCube = shape as Cube;
 
-                if (iceCube == null)
-                {
-                    Console.WriteLine("This shape is no cube");
-                }
+            //     if (iceCube == null)
+            //     {
+            //         Console.WriteLine("This shape is no cube");
+            //     }
 
-                if (shape is Cube)
-                {
-                    Console.WriteLine("This shape is a cube");
-                }
+            //     if (shape is Cube)
+            //     {
+            //         Console.WriteLine("This shape is a cube");
+            //     }
 
-                object cube1 = new Cube(7);
-                Cube cube2 = (Cube)cube1;
+            //     object cube1 = new Cube(7);
+            //     Cube cube2 = (Cube)cube1;
 
-                Console.WriteLine("{0} has a Volume of {1}", cube2.Name, cube2.Volume());
-            }
+            //     Console.WriteLine("{0} has a Volume of {1}", cube2.Name, cube2.Volume());
+            // }
 
             Console.ReadKey();
         }
