2fd0d2cb4bd7c10b5f698cb24856501e4cd3c2f7
Aman Ahmed <aman16.aa17@gmail.com>
2022-11-11 08:18:01 +0100

    Polymorphic Parameters

---
+++ 10 - Polymorphism/Audi.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10___Polymorphism
+{
+    internal class Audi : Car
+    {
+        private string brand = "Audi";
+        public string Model { get; set; }
+
+        public Audi(int hp, string color, string model) : base(hp, color)
+        {
+            Model = model;
+        }
+
+        public new void ShowDetails()
+        {
+            Console.WriteLine($"Brand: {brand}. Model: {Model}. HP: {HP}. Color: {Color}.");
+        }
+
+        public override void Repair()
+        {
+            Console.WriteLine("The Audi {0} was repaired.", Model);
+        }
+    }
+}
---
+++ 10 - Polymorphism/BMW.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10___Polymorphism
+{
+    internal class BMW : Car
+    {
+        private string brand = "BMW";
+        public string Model { get; set; }
+
+        public BMW(int hp, string color, string model) : base(hp, color)
+        {
+            Model = model;
+        }
+
+        public new void ShowDetails()
+        {
+            Console.WriteLine($"Brand: {brand}. Model: {Model}. HP: {HP}. Color: {Color}.");
+        }
+
+        public override void Repair()
+        {
+            Console.WriteLine("The BMW {0} was repaired.", Model);
+        }
+    }
+}
